4e55b9d915a116755ac95b18a26b4261eec4d4bd
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2022-11-21 09:43:18 +0700

    x

---
+++ menu/menu-xray.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+yl='\e[32;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+bd='\e[1m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+clear
+echo -e ""
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m            ⇱ MENU XRAY ⇲             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "❇️ 1. ADD ACCOUNT"
+echo -e "❇️ 2. DEL ACCOUNT"
+echo -e "❇️ 3. CEK STATUS"
+echo -e "❇️ 4. UPDATE MENU"
+echo -e "❇️ 5. SETTINGS MENU"
+echo -e "❇️ 6. Exit/Keluar"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m            ⇱ MENU XRAY ⇲             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+read -p "     Select From Options [1-69 or x] :  " menu
+case $menu in 
+1)
+menu-ssh
+;;
+2)
+menu-xray
+;;
+3)
+menu-cek
+;;
+4)
+menu-update
+;;
+5)
+menu-setting
+;;
+6)
+exit
+;;
+*)
+clear
+menu
+;;
+esac
