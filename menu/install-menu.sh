0f0b4a2ac0d3b1d9599f8e63d740abc2a989d5aa
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2022-11-21 09:45:11 +0700

    x

--- menu/install-menu.sh
+++ menu/install-menu.sh
@@ -4,12 +4,14 @@ cd /usr/bin
 
 wget -O menu "https://raw.githubusercontent.com/kuyaxxx/multi-port-tunneling/main/menu/menu.sh"
 wget -O traffic "https://raw.githubusercontent.com/kuyaxxx/multi-port-tunneling/main/menu/traffic.sh"
+wget -O traffic "https://raw.githubusercontent.com/kuyaxxx/multi-port-tunneling/main/menu/traffic.sh"
 
-wget -O add-xray "https://raw.githubusercontent.com/kuyaxxx/multi-port-tunneling/main/menu-xray/add-xray.sh"
+wget -O menu-xray "https://raw.githubusercontent.com/kuyaxxx/multi-port-tunneling/main/menu/menu-xray.sh"
 
 #
 chmod +x /usr/bin/menu
 chmod +x /usr/bin/traffic
+chmod +x /usr/bin/menu-xray
 chmod +x /usr/bin/add-xray
 
 #
